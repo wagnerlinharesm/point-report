d1cdcfdeb6e346c5b5ef1e573d4f53da7b3d5b60
GAPrado <guilhermeprado@outlook.com>
2024-03-21 00:11:26 -0300

    add import

---
+++ app/src/adapter/MailerAdapter.py
@@ -0,0 +1,35 @@
+import boto3
+from app.src.util.SingletonMeta import SingletonMeta
+
+
+class MailerAdapter(metaclass=SingletonMeta):
+    _ses_client = None
+
+    def __init__(self):
+        _ses_client = MailerAdapter.__get_ses()
+
+    @staticmethod
+    def __get_ses():
+        try:
+            boto3.client('ses')
+        except Exception as e:
+            raise Exception("failed to get ses client.", e)
+
+    def send(self, sender, destination, subject, text):
+        try:
+            response = self._ses_client.send_email(
+                Source=sender,
+                Destination={
+                    'ToAddresses': [destination]
+                },
+                Message={
+                    'Subject': {'Data': subject},
+                    'Body': {
+                        'Text': {'Data': text},
+                    }
+                }
+            )
+            print(response)
+        except Exception as e:
+            print("failed to send mail.", e)
+            raise Exception("failed to send mail.", e)
--- app/src/core/usecase/point_report_use_case.py
+++ app/src/core/usecase/point_report_use_case.py
@@ -1,15 +1,25 @@
 from app.src.util.SingletonMeta import SingletonMeta
 from app.src.adapter.WorkerAdapter import WorkerAdapter
 from app.src.adapter.PointAdapter import PointAdapter
+from app.src.adapter.MailerAdapter import MailerAdapter
 
 
 class PointReportUseCase(metaclass=SingletonMeta):
     _worker_adapter = WorkerAdapter()
     _point_adapter = PointAdapter()
+    _mailer_adapter = MailerAdapter()
 
     def execute(self, username, month, year):
         worker = self.__find_worker(username)
         points = self._point_adapter.find_all(worker.username, month, year)
+
+        self._mailer_adapter.send(
+            "test@test.com",
+            worker.mail,
+            "title",
+            "body :)"
+        )
+
         print(worker.username, worker.username)
         print(worker)
         print(points)
