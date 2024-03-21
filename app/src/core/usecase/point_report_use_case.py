4d106f4328dd8f5d1fce224ea131e29ee697710f
GAPrado <guilhermeprado@outlook.com>
2024-03-20 23:58:39 -0300

    add import

--- app/src/core/usecase/point_report_use_case.py
+++ app/src/core/usecase/point_report_use_case.py
@@ -7,9 +7,17 @@ class PointReportUseCase(metaclass=SingletonMeta):
     _worker_adapter = WorkerAdapter()
     _point_adapter = PointAdapter()
 
-    def execute(self, worker, month, year):
-        worker = self._worker_adapter.find_one(worker)
-        points = self._point_adapter.find_all(worker, month, year)
+    def execute(self, username, month, year):
+        worker = self.__find_worker(username)
+        points = self._point_adapter.find_all(worker.username, month, year)
         print(worker.username, worker.username)
         print(worker)
         print(points)
+
+    def __find_worker(self, username):
+        worker = self._worker_adapter.find_one(username)
+
+        if worker is None:
+            raise Exception("Worker '", username, "' not found.")
+
+        return worker
