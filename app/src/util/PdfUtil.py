fcdbdaef55447f95576e7faa724d2680b5d24f54
GAPrado <guilhermeprado@outlook.com>
2024-03-21 22:09:03 -0300

    add timeout, refactor to use proxy

--- app/src/util/PdfUtil.py
+++ app/src/util/PdfUtil.py
@@ -1,6 +1,9 @@
-import pdfkit
+from weasyprint import HTML
+from io import BytesIO
 
 class PdfUtil:
     @staticmethod
     def html_to_pdf(html):
-        return pdfkit.from_string(html, False)
+        pdf_bytes = BytesIO()
+        HTML(html).write_pdf(pdf_bytes)
+        return pdf_bytes
