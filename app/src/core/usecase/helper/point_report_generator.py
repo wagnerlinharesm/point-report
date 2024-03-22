763a6b32bdea6d1ec786df4f8b11a5cb2b3a8c62
wagnerlm <wagnerlink@gmail.com>
2024-03-22 15:55:55 -0300

    rollback

--- app/src/core/usecase/helper/point_report_generator.py
+++ app/src/core/usecase/helper/point_report_generator.py
@@ -1,48 +1,172 @@
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from io import BytesIO
+from string import Template
 
 
 class PointReportGenerator:
     @staticmethod
-    def generate_pdf(worker, points, month, year):
-        buffer = BytesIO()  # Buffer para armazenar o PDF
-        c = canvas.Canvas(buffer, pagesize=letter)  # Criando o objeto canvas
-
-        # Define o título do relatório e o adiciona ao PDF
-        title = f"Funcionário: {worker.username} - Mês/Ano: {month}/{year}"
-        c.drawString(30, 800, title)  # Posição do título no documento
-
-        # Variáveis para controlar a posição do texto
-        y_position = 780
-        line_height = 18
-
-        # Loop para adicionar informações de cada ponto no documento
-        for point in points:
-            y_position = PointReportGenerator.__add_point_to_canvas(c, point, y_position, line_height)
-
-            # Checagem para adicionar nova página se necessário
-            if y_position < 50:
-                c.showPage()
-                y_position = 800
-
-        c.showPage()
-        c.save()
-
-        buffer.seek(0)
-        return buffer.getvalue()  # Retorna o conteúdo do PDF em bytes
+    def generate(worker, points, month, year):
+        return PointReportGenerator.__build_html(worker, points, month, year)
 
     @staticmethod
-    def __add_point_to_canvas(canvas, point, y_position, line_height):
-        # Formata e adiciona um ponto (registro de presença) ao canvas
-        point_details = f"Dia: {point.date}, Situação: {point.situation}, Horas trabalhadas: {point.work_time}"
-        canvas.drawString(50, y_position, point_details)
-        y_position -= line_height
+    def __build_html(worker, points, month, year):
+        html_template = """
+            <!DOCTYPE html>
+            <html lang="en">
+            $head
+            $body
+            </html>
+        """
 
-        # Detalhes dos períodos
-        for period in point.periods:
-            y_position -= line_height
-            period_details = f"Entrada: {period.begin_time}, Saída: {period.end_time}, Horas: {period.work_time}"
-            canvas.drawString(60, y_position, period_details)
+        title = Template("Funcionário: $username - Mês/Ano: $month/$year").substitute({
+            "username": worker.username,
+            "month": month,
+            "year": year
+        })
 
-        return y_position  # Retorna a nova posição vertical após adicionar o ponto
+        head = PointReportGenerator.__build_head(title)
+        body = PointReportGenerator.__build_body(title, points)
+
+        return Template(html_template).substitute({
+            "head": head,
+            "body": body,
+        })
+
+    @staticmethod
+    def __build_head(title):
+        head_template = """
+            <head>
+                <meta charset="iso-8859-1">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>$title</title>
+                <style>
+                    table {
+                        border-collapse: collapse;
+                        width: 100%;
+                        margin-bottom: 20px;
+                    }
+                    th, td {
+                        border: 1px solid black;
+                        padding: 8px;
+                        text-align: left;
+                    }
+                    td:nth-child(1) {
+                        width: 33%;
+                    }
+                    td:nth-child(2) {
+                        width: 34%;
+                    }
+                    td:nth-child(3) {
+                        width: 33%;
+                    }
+                    .centered {
+                        text-align: center;
+                    }
+                    .point {
+                        background-color: #b2b2b2;
+                    }
+                    .period {
+                        background-color: #e2e2e2;
+                    }
+                </style>
+            </head>
+        """
+
+        return Template(head_template).substitute({
+            "title": title
+        })
+
+    @staticmethod
+    def __build_body(title, points):
+        body_template = """
+            <body>
+            <h2>$title</h2>
+            $elements
+            </body>
+        """
+
+        elements = PointReportGenerator.__build_points(points)
+
+        return Template(body_template).substitute({
+            "title": title,
+            "elements": elements
+        })
+
+    @staticmethod
+    def __build_points(points):
+        html_points = ""
+
+        for point in points:
+            html_points += PointReportGenerator.__build_point(point)
+
+        return html_points
+
+    @staticmethod
+    def __build_point(point):
+        element_template = """
+            <table>
+                <thead>
+                    <tr>
+                        <th colspan="3" class="centered point">Ponto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="centered"><b>Dia</b></td>
+                        <td class="centered"><b>Situação</b></td>
+                        <td class="centered"><b>Horas trabalhadas</b></td>
+                    </tr>
+                    <tr>
+                        <td>$date</td>
+                        <td>$situation</td>
+                        <td>$work_time</td>
+                    </tr>
+                </tbody>
+                <thead>
+                    <tr>
+                        <th colspan="3" class="centered period">Períodos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    $point_periods
+                </tbody>
+            </table>
+        """
+
+        point_periods = PointReportGenerator.__build_point_periods(point.periods)
+
+        return Template(element_template).substitute({
+            "date": point.date,
+            "situation": point.situation,
+            "work_time": point.work_time,
+            "point_periods": point_periods
+        })
+
+    @staticmethod
+    def __build_point_periods(point_periods):
+        html_point_periods = """
+            <tr>
+                <td class="centered"><b>Entrada</b></td>
+                <td class="centered"><b>Saida</b></td>
+                <td class="centered"><b>Horas trabalhadas</b></td>
+            </tr>
+        """
+
+        for point_period in point_periods:
+            html_point_periods += PointReportGenerator.__build_point_period(point_period)
+
+        return html_point_periods
+
+    @staticmethod
+    def __build_point_period(point_period):
+        point_period_template = """
+            <tr>
+                <td>$begin_time</td>
+                <td>$end_time</td>
+                <td>$work_time</td>
+            </tr>
+        """
+
+        return Template(point_period_template).substitute({
+            "begin_time": point_period.begin_time,
+            "end_time": point_period.end_time,
+            "work_time": point_period.work_time
+        })
