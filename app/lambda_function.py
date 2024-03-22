b23f6f594eb342f8cd75287a93d800e23c22105f
GAPrado <guilhermeprado@outlook.com>
2024-03-22 16:32:58 -0300

    add timeout, refactor to use proxy

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,52 +1,23 @@
 import json
 import base64
 
-from datetime import datetime
 from app.src.core.usecase.point_report_use_case import PointReportUseCase
 from app.src.util.JwtUtil import JwtUtil
 
 
 def handler(event, _):
-    print(event)
-    json_data = json.loads(event.get("Records", [])[0].get("body"))
-    print(json_data)
-
-    try:
-        raw_header = json_data['header']
-        print("raw", raw_header)
-        decoded_header = base64.b64decode(raw_header)
-        print("decoded_header", decoded_header)
-        header_str = decoded_header.decode('utf-8')
-        print("header_str", header_str)
-        header_json_str = header_str.replace("'", '"').replace('=', '":"').replace(', ', '","').replace('{', '{"').replace('}', '"}')
-        print("header_json_str", header_json_str)
-        header = json.loads(header_json_str)
-        print("header", header)
-    except Exception as e:
-        print('erro', e)
-
-    try:
-        raw_header = json_data['body']
-        print("raw", raw_header)
-        decoded_header = base64.b64decode(raw_header)
-        print("decoded_body", decoded_header)
-        header_str = decoded_header.decode('utf-8')
-        print("body_str", header_str)
-        header = json.loads(header_str)
-        print("body", header)
-    except Exception as e:
-        print('erro', e)
-
-    current_month, current_year = get_date()
+    current_month, current_year = get_date(event)
     username = get_username(event)
 
     point_report_use_case = PointReportUseCase()
     point_report_use_case.execute(username, current_month, current_year)
 
 
-def get_date():
-    current_date = datetime.now()
-    return current_date.month, current_date.year
+def get_date(event):
+    date_str = get_message_attribute(event, "body")
+    date = json.loads(date_str)
+
+    return date['month'], date['year']
 
 
 def get_username(event):
@@ -57,7 +28,7 @@ def get_username(event):
 
 
 def get_jwt_token(event):
-    authorization = get_body(event)
+    authorization = get_message_attribute(event, "authorization")
     split_authorization = authorization.split()
 
     if len(split_authorization) == 1:
@@ -68,18 +39,7 @@ def get_jwt_token(event):
     raise Exception("invalid authorization.")
 
 
-def get_body(event):
-    records = event.get("Records", [])
-
-    if len(records) == 0:
-        raise Exception("missing record.")
-
-    if len(records) > 1:
-        raise Exception("multiple records.")
-
-    body = records[0].get("body")
-
-    if body is None:
-        raise Exception("missing body.")
-
-    return body
+def get_message_attribute(event, name):
+    message = json.loads(event.get("Records", [])[0].get("body"))
+    b64attribute = message[name]
+    return base64.b64decode(b64attribute)
