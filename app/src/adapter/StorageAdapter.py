9a9a963d0743e01005ce0edac8462acf5a880242
GAPrado <guilhermeprado@outlook.com>
2024-03-21 23:29:10 -0300

    add timeout, refactor to use proxy

--- app/src/adapter/StorageAdapter.py
+++ app/src/adapter/StorageAdapter.py
@@ -40,5 +40,5 @@ class StorageAdapter(metaclass=SingletonMeta):
         return data
 
     def save_file(self, file_name, data):
-        bytes_data = BytesIO(data.encode('utf-8'))
+        bytes_data = BytesIO(data)
         self.s3_client.upload_fileobj(bytes_data, self.bucket_name, file_name)
